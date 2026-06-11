b55df06c700ded1a6f74ac6c656aa38e62a779ee
bhau <bhau@gmail.com>
2026-06-11 15:57:44 +0530

    Update homepage

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GitHub Actions Lab"
+    return "Hello from GitHub Actions Lab - Version 2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
